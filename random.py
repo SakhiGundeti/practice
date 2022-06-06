f7f3cf0ddd064b12fa709805baad531fd2ad5855
Sakhi <gundetisakhi@gmail.com>
2022-06-06 12:32:20 +0530

    Added a sum function

---
+++ random.py
@@ -0,0 +1,10 @@
+print("this is python")
+print("New line")
+#new change
+print("changes in a new branch")
+print("this is python")
+print("New line")
+sum = 79
+def summation():
+    sum = sum +1
+print(sum)
