6264ba04389500a9204e2eb02ffde61f5c46827d
Sakhi <gundetisakhi@gmail.com>
2022-06-07 11:45:06 +0530

    Deltion made

--- random.py
+++ random.py
@@ -1,13 +1,3 @@
 print("this is python")
 print("New line")
 print('Replace a line')
-print("changes in a new branch")
-print("this is python")
-print("New line")
-sum = 79
-def summation():
-    sum = sum +1
-print(sum)
-print("Chnages made in the trial branch")
-print("Let's merge it with the main branch")
-print("Are these visible in main?")
