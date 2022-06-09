05b7ceb20940d2ddc7f19da209abd6600fe5fcd3
Sakhi <gundetisakhi@gmail.com>
2022-06-09 19:05:14 +0530

    Changed the function

--- random.py
+++ random.py
@@ -1,10 +1,9 @@
-print("this is python")
-print("New line")
-print('Replace a line')
 n= 10
 sum = 0
-def sub:
-  while n>0:
-  sum  = sum + n
-  n = n - 1
+def sub():
+    while n>0:
+        sum  = sum + n
+        n = n - 1
+    print(n)
+    print(sum)
 print("Summation is",sum)
